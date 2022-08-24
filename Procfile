4946ac7e0df1779afa3159606d881bdf21e7fa64
Harish <harishpoctor@gmail.com>
2022-08-24 23:05:56 +0530

    Before deployment

---
+++ Procfile
@@ -0,0 +1,2 @@
+release: python manage.py migrate
+web: gunicorn hs_webapp_backend.wsgi
--- hs_webapp_backend/settings/prod.py
+++ hs_webapp_backend/settings/prod.py
@@ -1,7 +1,12 @@
 import os
+import dj_database_url
 from .common import *
 
 SECRET_KEY = os.environ['SECRET_KEY']
 
 DEBUG = False
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['hsapp-backend-prod.herokuapp.com']
+
+DATABASES = {
+  'default': dj_database_url.config()
+}
