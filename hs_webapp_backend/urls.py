1d7db888f6fb66cd405e5606ae018a42a0d73107
Harish <harishpoctor@gmail.com>
2022-09-06 17:53:28 +0530

    Adding template for root url

--- hs_webapp_backend/urls.py
+++ hs_webapp_backend/urls.py
@@ -15,10 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('__debug__/', include('debug_toolbar.urls')),
     path('admin/', admin.site.urls),
     path('api/', include('mindmaps.urls')),
     path('app/', include('core.urls')),
+    path('', TemplateView.as_view(template_name='home.html')),
 ]
